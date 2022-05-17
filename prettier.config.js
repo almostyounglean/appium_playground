e87c1564ab91ce2f14784ee2b144b364a6bcc739
Cherkashin Nikita <CherkashinNS@sibedge.com>
2022-05-17 11:55:49 +0700

    First setup

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tabWidt: 2,
+  printWidth: 200,
+  semi: true,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  bracketSpacing: true,
+};
---
+++ tests/basic_test.js
@@ -0,0 +1,3 @@
+Feature('basic');
+
+Scenario('test something', ({ I }) => {});
